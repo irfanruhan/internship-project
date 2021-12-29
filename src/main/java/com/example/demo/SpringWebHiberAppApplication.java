7bdc1357ed6683d511b06fbc9d580e986d5ddaf8
Irfan <86276066+irfanruhan@users.noreply.github.com>
2021-12-29 22:04:19 +0400

    Update SpringWebHiberAppApplication.java

--- src/main/java/com/example/demo/SpringWebHiberAppApplication.java
+++ src/main/java/com/example/demo/SpringWebHiberAppApplication.java
@@ -3,12 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @ComponentScan("com")
 public class SpringWebHiberAppApplication extends SpringBootServletInitializer{
 
